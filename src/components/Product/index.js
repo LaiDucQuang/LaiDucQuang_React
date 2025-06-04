00e4f248c06d4b7966847ce386793e19f8184c3a
Lại Đức Quang <lam788145@gmail.com>
2025-06-04 14:21:34 +0700

    rp2

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -23,9 +23,9 @@ function Product() {
         <>
             {/* Hiển thị danh sách sản phẩm */}
             <div className="product">
-                <Row gutter={[20, 20]}>
+                <Row gutter={[20, 20]} >
                     {products.map((item,_) => (
-                        <Col key={item.id} xxl={6} xl={6} lg={6} md={12} sm={12} xs={12}>
+                        <Col key={item.id} xxl={4} xl={6} lg={6} md={12} sm={12} xs={24}>
                             <ProductItem item={item}/>
                         </Col>
                     ))}
